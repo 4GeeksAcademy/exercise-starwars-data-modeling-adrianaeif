9bbcdcf5ef44d55ad0c76713c493b7ed00fd8259
Adriana Isea <adrianaisea02@gmail.com>
2024-09-10 03:24:26 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,32 +1,140 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    date = Column(Date, index=True)
+    
+    def to_dict(self):
+        return {}
+    
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    origin_planet = Column(Integer, ForeignKey("planet.id"))
+    height = Column(String(250), nullable=False)
+    mass = Column(Float, nullable=False)
+    unit_heigth = Column(String(250), nullable=False)
+    unit_mass = Column(String(250), nullable=False)
 
     def to_dict(self):
         return {}
+    
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    surface_water= Column(Integer, nullable=False)
+    diameter= Column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {}
+
+class vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    crew = Column(Integer, nullable=False)
+    passengers = Column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {}
+    
+class Favorite_Planet(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    planet = Column(Integer, ForeignKey("planet.id"))
+    
+
+    def to_dict(self):
+        return {}
+    
+class Favorite_Vehicle(Base):
+    __tablename__ = 'favorite_vehicles'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    vehicles = Column(Integer, ForeignKey("vehicles.id"))
+
+
+    def to_dict(self):
+        return {}
+    
+class Favorite_Character(Base):
+    __tablename__ = 'favorite_character'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    character = Column(Integer, ForeignKey("character.id"))
+
+    def to_dict(self):
+        return {}
+    
+class Favorite_Film(Base):
+    __tablename__ = 'favorite_film'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    pelicula = Column(Integer, ForeignKey("films.id"))
+
+    def to_dict(self):
+        return {}
+
+class Films(Base):
+    __tablename__ = 'films'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    year = Column(Date, index=True)
+    duration = Column(Integer, nullable=False)
+    director = Column(String(250), ForeignKey("director.id"))
+
+
+    def to_dict(self):
+        return {}
+    
+class Director(Base):
+    __tablename__ = 'director'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {}
+
+
+
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('User.id'))
+#     person = relationship(User)
+
+#     def to_dict(self):
+#         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
